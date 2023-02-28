75fb6ee90b118d48139e042d5030571512e6cb61
Rafał Prominski <prominski.rafal@gmail.com>
2023-02-28 21:26:58 +0100

    create importer decorator for importing transactions from file

--- src/main/scala/pitupitu/core/FileImporter.scala
+++ src/main/scala/pitupitu/core/FileImporter.scala
@@ -1,6 +1,11 @@
 package pitupitu.core
 
+import scala.io.Source
+import scala.util.Using
+
 class FileImporter(importer: Importer) extends Importer {
   def importTransactions(transactionsFile: String): Iterable[Transaction] =
-    ???
+    Using(Source.fromFile(transactionsFile)) { reader =>
+      importer.importTransactions(reader.mkString)
+    }.getOrElse(Seq.empty)
 }
