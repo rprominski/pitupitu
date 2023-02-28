1d1a526254e3cb3b508bff0f58ae756212b85291
Rafał Prominski <prominski.rafal@gmail.com>
2023-02-28 21:49:55 +0100

    add empty string test for degiro importer

--- src/test/scala/pitupitu/core/DegiroImporterTest.scala
+++ src/test/scala/pitupitu/core/DegiroImporterTest.scala
@@ -7,14 +7,15 @@ import java.time.LocalDate
 import scala.collection.mutable
 
 class DegiroImporterTest extends AnyFlatSpec with should.Matchers {
+  private val importer = DegiroImporter()
   private val transactions =
     """
       |Data,Czas,Produkt,ISIN,Giełda referenc,Miejsce wykonania,Liczba,Kurs,,Wartość lokalna,,Wartość,,Kurs wymian,Opłata transakcyjna,,Razem,,Identyfikator zlecenia
       |09-01-2020,09:05,VANGUARD S&P500,IE00B3XXRP09,EAM,XAMS,1,55.8810,EUR,-55.88,EUR,-55.88,EUR,,,,-55.88,EUR,56314f5e-bbdc-43b7-9474-1a595c82af97
       |09-01-2020,09:05,ISHARES S&P 500,IE0031442068,EAM,XAMS,1,29.3250,EUR,-29.33,EUR,-29.33,EUR,,,,-29.33,EUR,439ef09a-450c-40be-8c80-b0972d7cc822
       |""".stripMargin
+
   "Importer" should "import correct transactions file" in {
-    val importer = DegiroImporter()
     val expectedTransactions = List(
       Transaction(LocalDate.parse("2020-01-09"), "VANGUARD S&P500", 1, Price(BigDecimal(55.8810), "EUR"), "EAM", Price.EMPTY),
       Transaction(LocalDate.parse("2020-01-09"), "ISHARES S&P 500", 1, Price(BigDecimal(29.3250), "EUR"), "EAM", Price.EMPTY)
@@ -26,5 +27,11 @@ class DegiroImporterTest extends AnyFlatSpec with should.Matchers {
     for (transaction <- expectedTransactions) {
       importedTransactions.find(_ == transaction) shouldBe Some(transaction)
     }
+  }
+
+  it should "return empty list when importing from empty string" in {
+    val result = importer.importTransactions("")
+
+    result should have size 0
   }
 }
